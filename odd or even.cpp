7acfdac736fecaffb93d97bb8aa5c37fcabcfd31
Goushiikh <47105396+Goushiikh@users.noreply.github.com>
2019-02-14 16:27:13 +0530

    Create odd or even.cpp

---
+++ odd or even.cpp
@@ -0,0 +1,18 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+cout<<"enter the number";
+cin>>a;
+if(a%2==0)
+{
+cout<<"the number is even";
+}
+else
+{
+cout<<"the number is odd";
+}
+getch();
+}
